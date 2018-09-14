250c6dbb2d201d560320bf55d6b64dc17021d785
Piotr Swierszcz <elanssearts@gmail.com>
2018-09-14 14:55:25 +0100

    Levenshtein algorithm

---
+++ levenshteinarray.js
@@ -0,0 +1,27 @@
+let sentences = ["Hello, Im Piotr" , "How are You?" , "Wanna hang out?", "yeah sure"]
+let data = "How old are You?"
+
+const Levenshtein = () =>{
+    let result = [];
+    sentences.forEach((e) => {
+        let tmp;
+        let resoultObj = {},a = data;
+        let key = e
+        if (a.length > e.length) { tmp = a; a = e; e = tmp; }
+        let i, res, alen = a.length, elen = e.length, row = Array(alen);
+        for (i = 0; i <= alen; i++) { row[i] = i; }
+        for (let i = 1; i <= elen; i++) {
+            res = i;
+            for (let j = 1; j <= alen; j++) {
+                tmp = row[j - 1];
+                row[j - 1] = res;
+                res = e[i - 1] === a[j - 1] ? tmp : Math.min(tmp + 1, Math.min(res + 1, row[j] + 1));
+            }
+        }
+            resoultObj[key]=res
+        result.push(resoultObj)
+    });
+    return result
+}
+
+console.log(Levenshtein())
